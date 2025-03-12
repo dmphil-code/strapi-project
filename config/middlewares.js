a21023389481807ea2ff3012e91e30b3707d6b22
David Phillips <davidphillips@Davids-MacBook-Air.local>
2025-03-12 15:10:20 -0400

    Changes for strapi deployment

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,9 +1,15 @@
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      header: '*',
+      origin: ['http://localhost:3000', 'https://your-frontend-domain.netlify.app']
+    }
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
