a9907bdaaac4d8913416365d46548b305adff4db
David Phillips <davidphillips@Davids-MacBook-Air.local>
2025-10-27 16:31:04 -0400

    Updated CORS permissions

--- config/middlewares.js
+++ config/middlewares.js
@@ -17,7 +17,11 @@ module.exports = [
     name: 'strapi::cors',
     config: {
       enabled: true,
-      origin: ['https://blog.knotless.co', 'http://localhost:3000', 'https://knotless.co', 'https://knotless-v0-veltcr.flutterflow.app/'],
+      origin: ['https://blog.knotless.co', 'http://localhost:3000', 'https://knotless.co', 'https://knotless-v0-veltcr.flutterflow.app/',
+        'https://testing--knotless.netlify.app',
+        'https://knotless.netlify.app',
+        'https://*.netlify.app',
+      ],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       keepHeaderOnError: true,
